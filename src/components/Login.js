986837b1162af0ec96985bfd67902b9863e6eab9
Sufi <sufifisas@gmail.com>
2020-10-26 23:08:51 +0800

    saja

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,6 @@ class Login extends Component {
                     localStorage.setItem("id",response.data.id)
                     localStorage.setItem("token", response.data.token)
                     localStorage.setItem("type",response.data.type)
-                    localStorage.setItem("type",response.data.type)
                     this.setState({
                         loggedIn: true,
                         userIn: true
